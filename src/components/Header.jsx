641bbf8360cf5d40fc3c54fed29e8a7b017dca7f
ZiadBKL <86485110+Psychooz@users.noreply.github.com>
2024-11-23 14:55:48 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="mb-8 text-center">
     <h1 className="text-4xl font-bold text-white">QR Code Generator</h1>
-    <p className="text-gray-400 mt-2">Create sleek QR codes in dark mode</p>
+    <p className="text-gray-400 mt-2">Create and download QR codes with ease</p>
   </header>
   )
 }
